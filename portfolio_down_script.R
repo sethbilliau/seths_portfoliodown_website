8389ed10f2de9938b3f77470dfef76c23cd65ad8
Seth Billiau <sethbilliau@gmail.com>
2022-01-06 17:31:43 -0500

    Initial Commit
    Initial commit for data science website.

---
+++ portfolio_down_script.R
@@ -0,0 +1,19 @@
+# Install devtools if necessary
+if (!require('devtools')) install.packages('devtools')
+
+# Install portfoliodown from GitHub
+devtools::install_github("business-science/portfoliodown")
+
+# Import porfolio down
+library(portfoliodown)
+
+# Define project directory, create the directory if necessary, and set as wd 
+pathToDirectory = '/Users/sethbilliau/Desktop/portfoliodown'
+dir.create(file.path(pathToDirectory))
+setwd(pathToDirectory)
+
+# Create a new portfolio site
+portfoliodown::new_portfolio_site()
+
+
+portfoliodown::serve_site()
